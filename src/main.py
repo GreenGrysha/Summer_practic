b908ff9d66afde255d109facfa44ad871b7383d4
GreenGrysha <grushevskiyi25@gmail.com>
2025-06-29 19:35:22 +0700

    Функции для получения изображения были вынесены в отдельный файл

--- src/main.py
+++ src/main.py
@@ -1,83 +1,36 @@
-import os
-
+from ImageOperations.ImageDownload import ImageDownload
 import cv2
 from QT.Qt import QtInterface
 
-
-"""
-Пока что переменная 'img' будет реализована через global, в будущих версиях это исправится
-"""
-
-qt = QtInterface()
-img = None
-cap = None
-
-def img_not_none(func):
-    def wrapper(*args, **kwargs):
-        if img is None:
-            print("Не удалось загрузить изображение!")
-            return None
-        else:
-            return func(img, *args, **kwargs)
-    return wrapper
-
-
-def load_image():
-    global img
-    file_path, _ = qt.show_file_dialog()
-    if file_path:
-        img = cv2.imread(os.path.normpath(file_path))
-        if img is not None:
-            cv2.imshow("Загруженное изображение", img)
-        else:
-            print("Ошибка загрузки изображения!")
-
-
-def video():
-    global cap
-    if cap is not None:
-        print("Камера уже запущена!")
-        return
-    cap = cv2.VideoCapture(0)
-    if not cap.isOpened():
-        print("Ошибка: не удалось открыть камеру")
-        return
-    button = qt.add_button("Сделать снимок", lambda: save_frame())
-    qt.camera_layout.addWidget(button)
-    qt.camera_window.show()
-
-    while True:
-        ret, frame = cap.read()
-        if ret:
-            cv2.imshow('Камера (ESC для выхода)', frame)
-            key = cv2.waitKey(1)
-            if key == 27 or not qt.camera_window.isVisible():
-                break
-        else:
-            print("Ошибка чтения камеры")
-            break
-
-    button.deleteLater()
-    cv2.destroyWindow('Камера (ESC для выхода)')
-    cap.release()
-    cap = None
-    qt.camera_window.hide()
-
-def save_frame():
-    global img, cap
-    ret, frame = cap.read()
-    if ret:
-        current_img = frame.copy()
-        cv2.imshow("Сохранённый снимок", current_img)
-    else:
-        print("Не удалось сделать снимок!")
-
-def exit_app():
-    cv2.destroyAllWindows()
-    qt.app.quit()
-
-
 def main():
+
+    qt = QtInterface()
+    img = ImageDownload(qt)
+    current_image = None
+
+    def img_not_none(func):
+        def wrapper(*args, **kwargs):
+            if current_image is None:
+                print("Не удалось загрузить изображение!")
+                return None
+            else:
+                return func(img, *args, **kwargs)
+        return wrapper
+
+    def load_image():
+        nonlocal current_image
+        img.load_image()
+        current_image = img.img
+
+    def video():
+        nonlocal current_image
+        img.video()
+        current_image = img.img
+
+    def exit_app():
+        cv2.destroyAllWindows()
+        qt.app.quit()
+
     menu_items = [
         ("Загрузить изображение", load_image),
         ("Сделать снимок с камеры", video),
